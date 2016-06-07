a439c71ff69cbaf88d4653bf60fdd96bd3917da1
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2016-06-07 22:33:50 +0100

    Revert "Revert "Initiall commit""
    This reverts commit de7f8c1f8d52a8b2539905a5dbd9d33595bd4399.

---
+++ src/uk/fls/main/Painter.java
@@ -0,0 +1,61 @@
+package uk.fls.main;
+
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+import fls.engine.main.Init;
+import fls.engine.main.input.Input;
+import fls.engine.main.io.FileIO;
+import uk.fls.main.screens.PaintScreen;
+import uk.fls.main.util.SpriteParser;
+
+@SuppressWarnings("serial")
+public class Painter extends Init{
+
+	private static int w = 200;
+	private static int h = (w / 4) * 3;
+	private static int s = 3;
+	public Painter(){
+		super("FLS Painter V0.7", w * s, h * s);
+		useCustomBufferedImage(w, h, false);
+		setInput(new Input(this, Input.MOUSE));
+		setScreen(new PaintScreen());
+		skipInit();
+		
+		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		// Create a new blank cursor.
+		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+		    cursorImg, new Point(0, 0), "blank cursor");
+		// Set the blank cursor to the JFrame.
+		this.frame.getContentPane().setCursor(blankCursor);
+		
+		
+		int scale = 4;
+		BufferedImage icon = new BufferedImage(8 * scale, 8 * scale,BufferedImage.TYPE_INT_RGB);
+		SpriteParser sp = new SpriteParser(8, FileIO.instance.readInternalFile("/sheet2.art"));
+		
+		int[] d = sp.getData(1, 2);
+		int[] res = new int[32 * 32];
+		for(int i = 0 ; i <  8 * 8; i++){
+			int tx = i % 8;
+			int ty = i / 8;
+			
+			for(int j = 0; j < scale * scale; j++){
+				int dx = j % scale;
+				int dy = j / scale;
+				
+				int fx = (tx * scale) + dx;
+				int fy = (ty * scale) + dy;
+				res[fx + fy * (8 * scale)] = d[tx + ty * 8];
+			}
+		}
+		icon.setRGB(0, 0, 8 * scale, 8 * scale, res, 0, 8 * scale);
+		this.frame.setIconImage(icon);
+	}
+	
+	public static void main(String[] args){
+		new Painter().start();
+	}
+}
